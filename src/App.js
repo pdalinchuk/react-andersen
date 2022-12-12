86156663ffd9ef763ccf3e61af2a2e984a10b700
pdalinchuk <pavel.dalinchuk@gmail.com>
2022-12-12 21:27:02 +0300

    fix: migrating the Header component's title into a property

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import FormContainer from './components/form-container/form-container.component'
 function App() {
   return (
     <div className='App'>
-      <Header />
+      <Header title='Создание анкеты' />
       <FormContainer />
     </div>
   );
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,7 +3,7 @@ import './header.styles.css';
 
 class Header extends React.Component {
   render() {
-    return <h1 className='header'>Создание анкеты</h1>;
+    return <h1 className='header'>{this.props.title}</h1>;
   }
 }
 
